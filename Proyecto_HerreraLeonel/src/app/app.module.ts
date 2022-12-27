03ceffcf80a13dfb136572d4874dc6f6b6365c92
Leonel Herrera <113858983+LeoHrr@users.noreply.github.com>
2022-12-27 16:54:08 -0300

    -

--- Proyecto_HerreraLeonel/src/app/app.module.ts
+++ Proyecto_HerreraLeonel/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RedesComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
