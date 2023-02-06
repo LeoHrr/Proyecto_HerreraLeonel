b64cb6ebfb42aa39473fcb4733625146289c475f
Leonel Herrera <113858983+LeoHrr@users.noreply.github.com>
2023-02-06 17:58:31 -0300

    6-2-23

--- Proyecto_HerreraLeonel/src/app/app.module.ts
+++ Proyecto_HerreraLeonel/src/app/app.module.ts
@@ -6,17 +6,29 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RedesComponent } from './components/redes/redes.component';
 import { LogoComponent } from './components/logo/logo.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { FormacionComponent } from './components/formacion/formacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RedesComponent,
-    LogoComponent
+    LogoComponent,
+    AcercaDeComponent,
+    BannerComponent,
+    ExperienciaComponent,
+    FormacionComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
