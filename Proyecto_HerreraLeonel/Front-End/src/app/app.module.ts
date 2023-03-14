92c9476e7acaee50fa2c869ff8fa35402d59c538
Leonel Herrera <113858983+LeoHrr@users.noreply.github.com>
2023-03-13 22:12:47 -0300

    Conexión con database
    Se pasaron los parámetros de nombre y apellido mediante la base de datos.
    Se agregaron los componentes login y home.

--- Proyecto_HerreraLeonel/Front-End/src/app/app.module.ts
+++ Proyecto_HerreraLeonel/Front-End/src/app/app.module.ts
@@ -14,6 +14,9 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -27,12 +30,15 @@ import { FooterComponent } from './components/footer/footer.component';
     FormacionComponent,
     SkillsComponent,
     ProjectsComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({})
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
